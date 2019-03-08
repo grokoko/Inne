f4b70133c18137dccb3717d4fc2fcdcc4954dc0e
Konrad Lippert <45361699+grokoko@users.noreply.github.com>
2019-03-08 11:53:19 +0100

    Mini projekty z Codeacademy
    Dopasowane do Pythona 3 (kurs był na podstawie wersji 2giej).

---
+++ Madlibs.py
@@ -0,0 +1,43 @@
+# 
+STORY = "This morning %s woke up feeling %s. 'It is going to be a %s day!' Outside, a bunch of %ss were protesting to keep %s in stores. They began to %s to the rhythm of the %s, which made all the %ss very %s. Concerned, %s texted %s, who flew %s to %s and dropped %s in a puddle of frozen %s. %s woke up in the year %s, in a world where %ss ruled the world."
+
+
+print("Mad Libs has started")
+
+name = input("Enter a name: ")
+
+adjective1 = input("Enter an adjective 1: ")
+adjective2 = input("Enter an adjective 2: ")
+adjective3 = input("Enter an adjective 3: ")
+
+verb = input("Enter a verb: ")
+
+noun1 = input("Enter a noun 1: ")
+noun2 = input("Enter a noun 2: ")
+
+animal = input("Enter an animal: ")
+food = input("Enter a food: ")
+fruit = input("Enter a fruit: ")
+superhero = input("Enter a superhero: ")
+country = input("Enter a country: ")
+dessert = input("Enter a dessert: ")
+year = input("Enter a year: ")
+
+print("|STORY % (name, adjective1, adjective2, animal, food, verb, noun1, fruit, adjective3, name, superhero, name, country, name, dessert, name, year, noun2)")
+
+
+
+input("\n\nEnter")
+
+
+
+
+
+
+
+
+
+
+
+
+
